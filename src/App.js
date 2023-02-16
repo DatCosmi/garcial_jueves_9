86a52e9e23a064d7c4137b94250077bada93d2f3
Ivan Garcia <ivan03idk@gmail.com>
2023-02-16 09:27:04 -0600

    btns

--- src/App.js
+++ src/App.js
@@ -8,53 +8,37 @@ import Carrusel from "./components/Carrusel";
 import Curriculum from "./components/Curriculum";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Alumno from "./pages/Alumno";
+import Inicio from "./pages/Inicio";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/alumno">
-          <div className="container">
-            <h1>Carlos Iván García López</h1>
-          </div>
-        </Route>
-        <Route path="/carrera">
-          <div className="container">
-            <h1>Desarrollo de Software Multiplataforma</h1>
-          </div>
-        </Route>
-        <Route path="/universidad">
-          <div className="container">
-            <h1>Universidad Tecnológica de Durango</h1>
-          </div>
-        </Route>
-        <Route path="/logo">
-          <div className="logo">
-            <img src={logo} />
-          </div>
-        </Route>
-        <Route path="/foto">
-          <div className="foto">
-            <img src={foto} />
-          </div>
-        </Route>
-        <Route path="/mapa">
-          <Mapa />
-        </Route>
-        <Route path="/carrusel">
-          <Carrusel />
-          <br />
-          <Button variant="light" as={Link} to="/">
-            Regresar
-          </Button>
-        </Route>
-        <Route path="/curriculum">
-          <Curriculum />
-        </Route>
-      </Switch>
+      <div className="container mt-5">
+        <div className="btn-group">
+          <Link to="/Inicio" className="btn btn-warning">
+            INICIO
+          </Link>
+          <Link to="/Alumno" className="btn btn-dark">
+            ALUMNO
+          </Link>
+          <Link to="/Mapa" className="btn btn-dark">
+            MAPA
+          </Link>
+        </div>
+        <hr />
+        <Switch>
+          <Route path="/Inicio">
+            <Inicio />
+          </Route>
+          <Route path="/Alumno">
+            <Alumno />
+          </Route>
+          <Route path="/Mapa">
+            <Mapa />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
--- src/components/Mapa.jsx
+++ src/components/Mapa.jsx
@@ -7,12 +7,11 @@ export default class Mapa extends Component {
   render() {
     return (
       <div className="container">
-        <br />
         <h1>Mapa</h1>
         <br />
         <GoogleMaps
           apiKey={"AIzaSyBPDULIRm_tfXQDHNhR2RZvgWwwSvGFzTE"}
-          style={{ height: "350px", width: "570px" }}
+          style={{ height: "65vh", width: "70vw" }}
           zoom={12}
           center={{
             lat: 23.9901183,
@@ -21,9 +20,6 @@ export default class Mapa extends Component {
           markers={[{ lat: 23.9901183, lng: -104.6175808 }]}
         />
         <br />
-        <Button variant="light" as={Link} to="/">
-          Regresar
-        </Button>
       </div>
     );
   }
